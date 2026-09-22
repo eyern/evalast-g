87bddc08c28b433a848634aea02575d1b1fded62
eyern <eyern254@gmail.com>
2026-09-23 00:04:47 +0300

    nixpacks

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -140,6 +140,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
