cd4b07e4d004753207001c5722ead6fc7b3bc456
eyern <eyern254@gmail.com>
2026-09-23 06:37:54 +0300

    media root

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -138,7 +138,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, '/app/data')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
