6467d446a6e9f95bda2a5eede5ba09695b678aae
eyern <eyern254@gmail.com>
2026-09-23 10:06:06 +0300

    debug

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =  os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Allowed hosts - read from environment variable (comma-separated values)
 ALLOWED_HOSTS = ['evalastmotorcycles.com', 'https://evalastmotorcycles.com']
